8dea05c5be0f75b1f48a38358b4cf60929a54516
Lindsay Kinn <lindsay.kinn@gmail.com>
2020-11-24 17:29:58 -0500

    commit tree ea4fa640f08e728a9e3bcd36660fa5f8cef10824

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species, :say_species
+
+    @@all = []
+   
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @say_species = "I am a human."
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs 
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    @@all << Cat.new(name, self)
+    # Cat.all.each do |name|
+    # end
+  end
+
+  def buy_dog(name)
+    @@all << Dog.new(name, self)
+  end
+
+  def walk_dogs
+    #this calls on #dogs to return a specific dog that belongs to the owner
+    self.dogs.select {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.select {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.dogs.select {|dog| dog.mood = "nervous"} 
+    self.cats.select {|cat| cat.mood = "nervous"} 
+    self.dogs.select {|dog| dog.owner = nil}
+    self.cats.select {|cat| cat.owner = nil}    
+  end
+
+  def list_pets
+    "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
+    
+  end
+
 end
